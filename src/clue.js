1fca63dd87cf254ce43cb67391b9e5e7943e2294
Guilherme Silva <silva.gsg@gmail.com>
2021-08-12 12:46:48 +0100

    done

--- src/clue.js
+++ src/clue.js
@@ -1,31 +1,136 @@
 // ITERATION 1
 
-Alteration
-
 // Suspects Array
 
-const suspectsArray = [];
+let mrGreen = {
+  firstName: "Jacob",
+  lastName: "Green",
+  occupation: "Entrepreneur",
+  age: 45,
+  description: "He has a lot of connections",
+  image: "https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg",
+  color: "green",
+};
+
+let drOrchid = {
+  firstName: "Doctor",
+  lastName: "Orchid",
+  occupation: "Scientist",
+  age: 26,
+  description: "PhD in plant toxicology. Adopted daughter of Mr. Boddy",
+  image: "http://www.radiotimes.com/uploads/images/Original/111967.jpg",
+  color: "white",
+};
+
+let profPlum = {
+  firstName: "Victor",
+  lastName: "Plum",
+  occupation: "Designer",
+  age: 22,
+  description: "Billionaire video game designer",
+  image: "https://66.media.tumblr.com/ee7155882178f73b3781603f0908617c/tumblr_phhxc7EhPJ1w5fh03_540.jpg",
+  color: "purple",
+};
+
+let missScarlet = {
+  firstName: "Kasandra",
+  lastName: "Scarlet",
+  occupation: "Actor",
+  age: 31,
+  description: "She is an A-list movie star with a dark past",
+  image: "https://www.radiotimes.com/uploads/images/Original/111967.jpg",
+  color: "red",
+};
+
+let mrsPeacock = {
+  firstName: "Eleanor",
+  lastName: "Peacock",
+  occupation: "Socialité",
+  age: 36,
+  description: "She is from a wealthy family and uses her status and money to earn popularity",
+  image: "https://metrouk2.files.wordpress.com/2016/07/mrs-peacock.jpg",
+  color: "blue",
+};
+
+let mrMustard = {
+  firstName: "Jack",
+  lastName: "Mustard",
+  occupation: "Retired Football player",
+  age: 62,
+  description: "He is a former football player who tries to get by on his former glory",
+  image: "https://static.independent.co.uk/s3fs-public/thumbnails/image/2016/07/04/08/unspecified-3.jpg",
+  color: "yellow",
+};
+
+const suspectsArray = [
+  mrGreen, //0
+  drOrchid, //1
+  profPlum, //2
+  missScarlet, //3
+  mrsPeacock, //4
+  mrMustard, //5 suspectsArray.length = 6
+];
+
 
 // Rooms Array
 
-const roomsArray = [];
+const roomsArray = [
+  {name: "Dining Room"}, //0
+  {name: "Conservatory"}, //1
+  {name: "Kitchen"}, //2
+  {name: "Study"}, //3
+  {name: "Library"}, //4
+  {name: "Billiard Room"}, //5
+  {name: "Lounge"}, //6
+  {name: "Ballroom"}, //7
+  {name: "Hall"}, //8
+  {name: "Spa"}, //9
+  {name: "Living Room"}, //10
+  {name: "Observatory"}, //11
+  {name: "Theater"}, //12
+  {name: "Guest House"}, //13
+  {name: "Patio"}, //14 roomsArray.length = 15
+];
 
 // Weapons Array
 
-const weaponsArray = [];
+const weaponsArray = [
+  {name: "rope", weight: 10}, //0
+  {name: "knife", weight: 8}, //1
+  {name: "candlestick", weight: 2}, //2
+  {name: "dumbbell", weight: 30}, //3
+  {name: "poison", weight: 2}, //4
+  {name: "axe", weight: 15}, //5
+  {name: "bat", weight: 13}, //6
+  {name: "trophy", weight: 25}, //7
+  {name: "pistol", weight: 20}, //8 weaponsArray.length = 9
+];
 
 
 // ITERATION 2
 
-function selectRandom() {}
+function selectRandom(array) {
+  let randomIndex = Math.floor(Math.random() * array.length);
+  return array[randomIndex];
+}
 
-function pickMystery() {}
+function pickMystery() {
+  let suspectValue = selectRandom(suspectsArray);
+  let weaponValue = selectRandom(weaponsArray);
+  let roomValue = selectRandom(roomsArray);
+  return {suspect: suspectValue, weapon: weaponValue, room: roomValue,}
+}
 
 
 // ITERATION 3
 
-function revealMystery() {}
-
+function revealMystery(envelope) {
+  let murderFirstName = envelope.suspect.firstName;
+  let murderLasttName = envelope.suspect.lastName;
+  let weaponUsed = envelope.weapon.name;
+  let roomUsed = envelope.room.name;
+  return `${murderFirstName} ${murderLasttName} killed Mr. Boddy using the ${weaponUsed} in the ${roomUsed}!`;
+}
 
 
 // The following is required to make unit tests work.
